508b3130ea2190a51b0cdd581f2202b361f88ea1
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-05-20 15:33:27 +0200

    Fetch class results on client

--- src/routes/results/[className].svelte
+++ src/routes/results/[className].svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   interface Result {
     place: string;
     name: string;
@@ -10,7 +12,22 @@
     start: number;
   }
 
-  export let results: Result[];
+  export let className;
+
+  let results: Result[] = [];
+
+  onMount(async () => {
+    const method = 'getclassresults';
+    const comp = '22636';
+    const response = await fetch(
+      [
+        'https://liveresultat.orientering.se/api.php',
+        [`method=${method}`, `comp=${comp}`, `class=${className}`].join('&')
+      ].join('?')
+    );
+
+    ({ results } = await response.json());
+  });
 </script>
 
 {#each results as { place, name, result }}
--- src/routes/results/[className].ts
+++ src/routes/results/[className].ts
@@ -1,11 +1,3 @@
 export async function get({ params }: { params: { className: string } }) {
-  const method = 'getclassresults';
-  const comp = '22636';
-  const response = await fetch(
-    [
-      'https://liveresultat.orientering.se/api.php',
-      [`method=${method}`, `comp=${comp}`, `class=${params.className}`].join('&')
-    ].join('?')
-  );
-  return { body: await response.json() };
+  return { body: params };
 }
