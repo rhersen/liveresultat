b3cb5587eda7c31c2a3e527604e08925b01476a7
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-05-30 08:29:05 +0200

    prerender competitions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
   import type { Competition } from '../types';
 
   export const hydrate = false;
+  export const prerender = true;
   export const router = false;
 
   export let competitions: Competition;
